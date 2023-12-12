b520399d93e8fa8eb84ca7f377d6b420c12b5efd
CamiloCortesM <camilo.cortes1753@gmail.com>
2023-12-12 02:18:45 -0500

    add new interface

--- main.py
+++ main.py
@@ -1,38 +1,74 @@
 import tkinter as tk
 from tkinter import ttk
 from experta import *
+from PIL import Image, ImageTk
 from rules import ExpertSystem, Diagnosis
 
 class Interface(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
+        self.check_vars = {}
+        self.backgroundcolor = "#fdefb0"
+        self.configure(bg=self.backgroundcolor)
+        
+        self.check_var_humo = tk.BooleanVar()
+        self.check_var_luces = tk.BooleanVar()
+        self.check_var_arranque = tk.BooleanVar()
+        self.check_var_velocidad = tk.BooleanVar()
+        self.check_var_vibracion = tk.BooleanVar()
+        self.check_var_llantas = tk.BooleanVar()
+        
         self.title("Sistema Experto Automotriz")
-        self.geometry("400x300")
+        self.geometry("350x700")
 
-        self.label = tk.Label(self, text="Seleccione los problemas del automóvil:")
-        self.label.pack(pady=10)
+        self.label = tk.Label(self, text="Seleccione los problemas del automóvil:", bg=self.backgroundcolor)
+        self.label.grid(pady=10)
 
-        self.comboboxes = {}
+        self.checkboxes={}
         symptoms = ["motor_ruidoso", "frenos_ineficientes", "luces_fallando", "problemas_combustible", "problemas_arranque", "vibraciones_al_frenar", "pérdida_de_potencia_del_motor", "temperatura_del_motor_elevada", "ruidos_anormales_al_girar_la_dirección", "consumo_excesivo_de_combustible", "fugas_de_líquidos_bajo_el_vehículo", "desgaste_desigual_de_los_neumáticos"]
-        for symptom in symptoms:
-            frame = tk.Frame(self)
-            frame.pack(pady=5)
-            label = tk.Label(frame, text=f"{symptom.capitalize().replace('_', ' ')}:")
-            label.pack(side=tk.LEFT)
-            combobox = ttk.Combobox(frame, values=["no", "si"], state="readonly")
-            combobox.pack(side=tk.RIGHT)
-            self.comboboxes[symptom] = combobox
 
-        self.diagnosis_label = tk.Label(self, text="")
-        self.diagnosis_label.pack(pady=10)
+        style = ttk.Style(self)
+        style.configure("TCheckbutton", background=self.backgroundcolor)
 
-        self.diagnose_button = tk.Button(self, text="Diagnosticar", command=self.diagnose)
-        self.diagnose_button.pack(pady=10)
+        for i, symptom in enumerate(symptoms, start=2):
+            self.checkboxes[symptom] = tk.BooleanVar()
+            
+            checkbox = ttk.Checkbutton(
+                self, 
+                text=symptom.replace('_', ' '), 
+                variable=self.checkboxes[symptom],
+                style="TCheckbutton",
+                takefocus=0
+            )
+            checkbox.grid(row=i, column=0, padx=10, pady=5, sticky=tk.W)
 
+        self.diagnosis_label = tk.Label(self, text="", bg=self.backgroundcolor)
+        self.diagnosis_label.grid(row=i+2, column=0, columnspan=2, pady=5)
+
+        self.diagnose_button = tk.Button(
+            self, 
+            text="Diagnosticar", 
+            command=self.diagnose, 
+            bg="#ff6745", 
+            fg="black",   
+        )
+        self.diagnose_button.grid(row=i+1, column=0, columnspan=2, pady=10)
+        
+        
+        image = Image.open("./taller-mecanica.jpg") 
+        image = image.resize((350, 200))
+        banner_image = ImageTk.PhotoImage(image)
+
+        self.banner_label = tk.Label(self, image=banner_image, bg=self.backgroundcolor)
+        self.banner_label.image = banner_image
+        self.banner_label.grid(row=17, column=0, columnspan=2, pady=10)
+                
     def diagnose(self):
-        selected_symptoms = {symptom: self.comboboxes[symptom].get().lower() for symptom in self.comboboxes}
+        selected_symptoms_booleans = {symptom: self.checkboxes[symptom].get() for symptom in self.checkboxes}
+        
+        selected_symptoms = {clave: 'si' if valor else 'no' for clave, valor in selected_symptoms_booleans.items()}
         engine = ExpertSystem()
-
+        
         for symptom, response in selected_symptoms.items():
             engine.declare(Fact(**{symptom: response}))
 
